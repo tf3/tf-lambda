45d3ed967bea840b7f627dd7e7e4d15e6c539b51
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2020-02-14 13:56:21 -0500

    Update config

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const gitToken = process.env.GIT_TOKEN;
 if (!gitToken) throw new Error('GitHub auth token not set');
 
 module.exports = {
-  targetPath: './tmp',
+  targetPath: '/tmp',
   repoPath: `https://tf3:${gitToken}@github.com/tf3/vaccha/archive/master.zip`,
-  zipDownloadPath: './repo.zip',
-  contentPath: './tmp/vaccha-master/content',
+  zipDownloadPath: '/tmp/repo.zip',
+  contentPath: '/tmp/vaccha-master/content',
   baseURL: 'http://thomasfoerster.ca',
   bucketName: 'alpha-lambda-01',
   targetFileName: 'index-01.html'
