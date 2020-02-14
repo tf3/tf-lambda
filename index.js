c790eb1a8d16d001ec4524286e2efb427108aab1
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2020-02-14 09:22:53 -0500

    Create Lambda handler

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const getMetadataOfNewestPost = require('./getMetadataOfNewestPost');
 const createHTMLFromMetadata = require('./createHTMLFromMetadata');
 const writeHTMLToS3Bucket = require('./writeHTMLToS3Bucket');
 
-clone(repoPath, targetPath)
-  .then(getMetadataOfNewestPost)
-  .then(createHTMLFromMetadata)
-  .then(writeHTMLToS3Bucket)
-  .then(console.log)
-  .catch(console.error);
+// TODO: check event to ensure it's actually from the webhook
+
+exports.handler = (event, context) => (
+  clone(repoPath, targetPath)
+    .then(getMetadataOfNewestPost)
+    .then(createHTMLFromMetadata)
+    .then(writeHTMLToS3Bucket)
+    .then(res => { console.log(res); return res; })
+    .catch(console.error)
+);
