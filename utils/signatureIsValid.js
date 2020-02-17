ce8a57a02dc616f11fc51dbde96cd65e0a894a8f
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2020-02-17 12:52:32 -0500

    Make changes nessary for Proxy Integration

--- utils/signatureIsValid.js
+++ utils/signatureIsValid.js
@@ -8,11 +8,10 @@ const signatureIsValid = event => {
   const { body } = event;
   const signature = event.headers['X-Hub-Signature'];
 
-  const payload = JSON.stringify(body);
-  if (!payload || !signature) return false;
+  if (!body || !signature) return false;
 
   const hmac = crypto.createHmac('sha1', webhookSecret);
-  const digest = Buffer.from('sha1=' + hmac.update(payload).digest('hex'), 'utf8');
+  const digest = Buffer.from('sha1=' + hmac.update(body).digest('hex'), 'utf8');
 
   const checksum = Buffer.from(signature, 'utf8');
   if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
