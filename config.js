f1e1b5cd5862763482bd20f0245e3ebc1455b043
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2020-02-17 10:10:41 -0500

    Clarify comments

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   zipExtractPath: '/tmp', // where the zip file's contents will be extracted to
   repoPath: `https://tf3:${gitToken}@github.com/tf3/vaccha/archive/master.zip`,
   zipDownloadPath: '/tmp/repo.zip', // where the zip file will be downloaded to
-  contentPath: '/tmp/vaccha-master/content', // path to the blog's contents
+  contentPath: '/tmp/vaccha-master/content', // path to the extracted blog's contents
   blogUrl: 'https://vaccha.com',
   bucketName: 'alpha-lambda-01',
   targetFileName: 'index-02.html' // name of bucket file that will be created or updated
